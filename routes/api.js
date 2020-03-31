998f8b5d5a6105853d44e9f28f04ceb22843bbd8
thanghd <thanghd@bap.jp>
2020-03-31 17:29:43 +0700

    [API] add table Users in mongo.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
 const express = require('express')
-
+const authValidation = require('../app/Policies/Validation/authValidation')
+const checkMiddleware = require('../app/Policies/Middleware/checkMiddleware')
 const ok = require('../app/Responses/ok')
 const router = express.Router()
 
 router.get('/health-check', (req, res) => ok(res))
+
+// Authentication
+router.route('/register').post(authValidation.register)
 module.exports = router
